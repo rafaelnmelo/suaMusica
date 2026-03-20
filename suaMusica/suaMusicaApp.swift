653e50acb6fa4f49d9af7eb16952ffb885692078
Rafael Melo <rafaelnunesmelo@gmail.com>
2026-03-20 11:32:50 -0300

    Creating Xcode project

---
+++ suaMusica/suaMusicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  suaMusicaApp.swift
+//  suaMusica
+//
+//  Created by Rafael Nunes on 20/03/26.
+//
+
+import SwiftUI
+
+@main
+struct suaMusicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
